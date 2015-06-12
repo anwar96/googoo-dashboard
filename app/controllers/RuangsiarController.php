77f38ab3cd9d55b36af7386e27484aca3d3cf276
Nur anwar <rawnaku@gmail.com>
2015-06-12 11:16:24 +0700

    rejected artist tidak ditampilkan di ruangsiar di panel artist no song

--- app/controllers/RuangsiarController.php
+++ app/controllers/RuangsiarController.php
@@ -1,10 +1,8 @@
 <?php
 
-class RuangsiarController extends BaseController
-{
+class RuangsiarController extends BaseController {
 
-    public function getIndex()
-    {
+    public function getIndex() {
         $date = App::environment() == 'local' ? '2014-09-25' : date('Y-m-d');
         $date = date('Y-m-d');
         $programs = Program::all();
@@ -12,30 +10,31 @@ class RuangsiarController extends BaseController
         $nosongs = $this->nosong($currentProgram->id, $date);
 
         return View::make('ruangsiar.getIndex')
-                ->with('currentProgram', $currentProgram)
-                ->withNosongs($nosongs)
-                ->withPrograms($programs);
+            ->with('currentProgram', $currentProgram)
+            ->withNosongs($nosongs)
+            ->withPrograms($programs);
     }
 
-    private function nosong($programID, $date)
-    {
+    private function nosong($programID, $date) {
         $sql = "
-            SELECT 
+            SELECT
                 a.id,a.name,count(*) as total
             FROM listeners l
                 LEFT JOIN music_interests mi ON mi.fb_user_id = l.facebook_id
                 LEFT JOIN artists a ON a.id = mi.artist_id
                 LEFT JOIN songs s ON s.artist_id = mi.artist_id
-            WHERE 
+                LEFT JOIN rejected_artists ra ON ra.artist_id = a.id
+            WHERE
                 l.program_id = ?
-                AND DATE(l.created_at) = ? 
+                AND DATE(l.created_at) = ?
                 AND a.name IS NOT  NULL
                 AND s.id IS NULL
+                AND ra.id IS NULL
             GROUP BY a.id
-            ORDER BY total DESC  
+            ORDER BY total DESC
             LIMIT 10
         ";
-        
+
         $results = DB::select($sql, array($programID, $date));
         return $results;
     }
